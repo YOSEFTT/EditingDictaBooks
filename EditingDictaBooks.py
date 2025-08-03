5b1dc55f1ac69be14b7b85f9a41e3fdb21e0fe28
YOSEFTT <huxhtct@gmail.com>
2025-08-04 00:32:48 +0300

    עדכון מספר גירסא

--- EditingDictaBooks.py
+++ EditingDictaBooks.py
@@ -50,7 +50,7 @@ from urllib3.util.ssl_ import create_urllib3_context
 from bidi.algorithm import get_display
 
 # גרסת התוכנה - משתנה גלובלי מרכזי
-VERSION = "3.6.0"
+VERSION = "3.7.0"
 
 # מזהה ייחודי לאפליקציה
 myappid = 'MIT.LEARN_PYQT.dictatootzaria'
